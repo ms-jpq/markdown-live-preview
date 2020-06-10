561ede2db7f6b459568a6ef39c5bb51028a476cb
Fly me to the moon <git@bigly.dog>
2020-06-09 17:07:59 -0700

    render

--- md.py
+++ md.py
@@ -4,8 +4,8 @@ from argparse import ArgumentParser, Namespace
 from http.server import ThreadingHTTPServer
 from os import getcwd, environ
 from os.path import basename, dirname, isdir, join, splitext
-from typing import Any, Callable, Dict, List
 from sys import stderr
+from typing import Any, Callable, Dict, List
 
 
 try:
@@ -26,8 +26,13 @@ except ImportError:
     run(["pip", "install", "jinja2", "markdown"])
 
 
-from jinja2 import Environment, FileSystemLoader,  StrictUndefined
+from jinja2 import Environment, FileSystemLoader, Template, StrictUndefined
 from markdown import markdown
+
+
+__dir__ = dirname(__file__)
+__templates__ = join(__dir__, "templates")
+__index_html__ = "index.html.j2"
 
 
 def parse_args() -> Namespace:
@@ -47,11 +52,6 @@ def build_j2(src: str, filters: Dict[str, Callable] = {}) -> Environment:
   return j2
 
 
-# def render_j2(source: str, values: Dict[str, Any]) -> str:
-#   rendered = j2.get_template(template).render(**values)
-#   return rendered
-
-
 def slurp(name) -> str:
   with open(name) as fd:
     return fd.read()
@@ -62,14 +62,19 @@ def md_html(name: str) -> str:
   return markdown.markdown(md)
 
 
+def render(template: Template, values: Dict[str, Any]) -> str:
+  rendered: str = template.render(**values)
+  return rendered
+
+
 def srv(name: str) -> str:
   return 2
 
 
 def main() -> None:
   args = parse_args()
-  templates = join(dirname(__file__), "templates")
-  j2 = build_j2(templates)
+  j2 = build_j2(__templates__)
+  index_template = j2.get_template(__index_html__)
   md_html(args.markdown)
 
 
