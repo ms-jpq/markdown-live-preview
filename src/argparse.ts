d907d4c6f38f407098ea08fc11ba4fc93a19cd14
Fly me to the moon <git@bigly.dog>
2020-06-16 20:51:50 -0700

    argparse

---
+++ src/argparse.ts
@@ -0,0 +1,37 @@
+import { Command } from "commander"
+import { isfile, slurp } from "nda/dist/node/fs"
+import { dirname, join } from "path"
+
+export type Arguments = {
+  markdown: string
+  port: number
+}
+
+export const argparse = async (): Promise<Arguments> => {
+  const prog = new Command()
+  prog.storeOptionsAsProperties(false)
+
+  const pkg_data = await slurp(join(dirname(__dirname), "package.json"))
+  const pkg_info = JSON.parse(pkg_data)
+  prog.name(pkg_info["name"])
+  prog.version(pkg_info["version"])
+
+  prog.arguments("<markdown>")
+  prog.option("-p, --port <port>", "PORT", Number, 8080)
+
+  await prog.parseAsync(process.argv)
+  if (prog.args.length != 1) {
+    console.error(prog.helpInformation())
+    process.exit(1)
+  }
+  const [markdown] = prog.args
+  const args = prog.opts()
+
+  if (!(await isfile(markdown))) {
+    console.error(`Not a file -- ${markdown}`)
+    process.exit(1)
+  }
+
+  return { ...args, markdown } as Arguments
+}
+
--- src/entry.ts
+++ src/entry.ts
@@ -1,16 +1,10 @@
 #!/usr/bin/env ts-node
 
-import { Command } from "commander"
-import { slurp } from "nda/dist/node/fs"
-import { dirname, join } from "path"
+import { argparse } from "./argparse"
 
 const main = async () => {
-  const prog = new Command()
-  const pkg_data = await slurp(join(dirname(__dirname), "package.json"))
-  const pkg_info = JSON.parse(pkg_data)
-  const ver = pkg_info["version"]
-  prog.version(ver)
-  prog.parse(process.argv)
+  const args = await argparse()
+  console.log(args)
 }
 
 main()
