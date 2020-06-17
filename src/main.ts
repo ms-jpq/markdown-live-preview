7cc3745557c79f05c4e4b82e1c4c4f969862c194
Fly me to the moon <git@bigly.dog>
2020-06-17 00:05:04 -0700

    server

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 
+import { hostname } from "os"
 import { slurp } from "nda/dist/node/fs"
 import { argparse } from "./argparse"
 import { watch } from "./watch"
 import { render } from "./render"
 import { serve } from "./server"
+
 
 const main = async () => {
   const args = await argparse()
@@ -14,20 +16,28 @@ const main = async () => {
     interval: args.interval,
   })
 
+  let page = ""
+
   ;(async () => {
     for await (const _ of mon) {
       const markdown = await slurp(args.markdown)
       const html = render(markdown)
+      page = html
+      console.log(`Updated -- ${new Date()}`)
     }
     console.error(`File Moved -- ${args.markdown}`)
     process.exit(1)
   })()
-  await serve({
-    port: args.port,
-    html: {},
-    js: {},
-    css: {},
-  })
+
+  await (async () => {
+    console.log(`Serving -- http://${hostname()}:${args.port}`)
+    await serve({
+      port: args.port,
+      html: () => page,
+      js: "js-stub",
+      css: "css-stub",
+    })
+  })()
 }
 
 main()
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,31 @@
+import cors from "cors"
+import express from "express"
+
 export type ServerOpts = {
   port: number
-  html: Record<string, string>
-  js: Record<string, string>
-  css: Record<string, string>
+  html: () => string
+  js: string
+  css: string
 }
 
-export const serve = async ({ port, html, js, css }: ServerOpts) => {}
+export const serve = async ({ port, html, js, css }: ServerOpts) => {
+  const srv = express().use(cors())
+
+  srv.get(/\.js/, (_, resp) => {
+    resp.type("text/javascript")
+    resp.send(js)
+  })
+
+  srv.get(/\.css$/, (_, resp) => {
+    resp.type("text/css")
+    resp.send(css)
+  })
+
+  srv.get("*", (_, resp) => {
+    resp.type("text/html")
+    resp.send(html())
+  })
+
+  srv.listen(port)
+}
 
