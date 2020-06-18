3adc6c872ddf95ff7cf23d73060eda7d7b6ee05f
Fly me to the moon <git@bigly.dog>
2020-06-17 17:07:58 -0700

    added shallow diff

--- src/server/reconciliate.ts
+++ src/server/reconciliate.ts
@@ -1,15 +1,32 @@
 import { JSDOM } from "jsdom"
+import { reduce } from "nda/dist/isomorphic/iterator"
 
-const diff_pair = (prev: HTMLElement, next: HTMLElement) => {
+const p_attrs = (attrs: NamedNodeMap) =>
+  reduce((a, { name, value }) => Object.assign(a, { [name]: value }), {}, attrs)
+
+const diff_shallow = (prev: HTMLElement, next: HTMLElement) => {
   if (prev.tagName !== next.tagName) {
     return true
-  } else if (prev.children.length !== next.children.length) {
-    return true
-  } else if (prev.textContent !== next.textContent) {
-    return true
-  } else {
-    return false
   }
+  const pa = Object.entries(p_attrs(prev.attributes))
+  const na = Object.entries(p_attrs(next.attributes))
+  for (const [p, v] of pa) {
+    if (na[p] !== v) {
+      return true
+    }
+    Reflect.deleteProperty(na, p)
+  }
+  for (const [p, v] of na) {
+    if (pa[p] !== v) {
+      return true
+    }
+  }
+  const pc = prev.classList.value
+  const nc = next.classList.value
+  if (pc !== nc) {
+    return true
+  }
+  return false
 }
 
 export const reconciliate = (prev: JSDOM | undefined, next: string) => {
