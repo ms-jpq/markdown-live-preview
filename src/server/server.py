44ec01abed58adc4ad535b82a72420c15a233cef
Fly me to the moon <git@bigly.dog>
2020-07-03 19:41:15 -0700

    async jobs

--- src/server/server.py
+++ src/server/server.py
@@ -1,12 +1,13 @@
+from asyncio import Task, create_task
 from dataclasses import dataclass
-from typing import AsyncIterator
+from typing import AsyncIterator, Awaitable, Callable, List
+from weakref import WeakSet
 
 from aiohttp.web import (
     Application,
     AppRunner,
     TCPSite,
     WebSocketResponse,
-    WSMsgType,
     get,
     middleware,
 )
@@ -19,7 +20,13 @@ HEARTBEAT_TIME = 1
 
 @dataclass
 class Payload:
+    title: str
     markdown: str
+
+
+@dataclass
+class Update:
+    sha: str
 
 
 @middleware
@@ -29,22 +36,40 @@ async def cors(request: Request, handler: _Handler) -> StreamResponse:
     return resp
 
 
-async def serve(
-    localhost: bool, port: int, root: str, gen: AsyncIterator[Payload]
-) -> None:
+def build(
+    localhost: bool,
+    port: int,
+    root: str,
+    payloads: AsyncIterator[Payload],
+    updates: AsyncIterator[Update],
+) -> Callable[[], Awaitable[None]]:
     host = "localhost" if localhost else "0.0.0.0"
+    websockets: WeakSet[WebSocketResponse] = WeakSet()
+    jobs: List[Task] = []
 
     async def ws_resp(request: BaseRequest) -> WebSocketResponse:
         ws = WebSocketResponse(heartbeat=HEARTBEAT_TIME)
         await ws.prepare(request)
+        websockets.add(ws)
 
         async for msg in ws:
-            if msg.type == WSMsgType.text:
-                await ws.send_json({})
-            elif msg.type == WSMsgType.close:
-                break
+            print(msg)
+            pass
 
         return ws
+
+    async def broadcast() -> None:
+        async for update in updates:
+            for ws in websockets:
+                ws.send_json(update)
+
+    async def start_jobs(app: Application) -> None:
+        b_task = create_task(broadcast())
+        jobs.append(b_task)
+
+    async def stop_jobs(app: Application) -> None:
+        for job in jobs:
+            job.cancel()
 
     normalize = normalize_path_middleware()
     middlewares = (normalize, cors)
@@ -53,8 +78,13 @@ async def serve(
     app = Application(middlewares=middlewares)
     app.add_routes(routes)
     app.router.add_static(prefix="/", path=root)
+    app.on_startup.append(start_jobs)
+    app.on_cleanup.append(stop_jobs)
 
-    runner = AppRunner(app)
-    await runner.setup()
-    site = TCPSite(runner, host=host, port=port)
-    await site.start()
+    async def start() -> None:
+        runner = AppRunner(app)
+        await runner.setup()
+        site = TCPSite(runner, host=host, port=port)
+        await site.start()
+
+    return start
