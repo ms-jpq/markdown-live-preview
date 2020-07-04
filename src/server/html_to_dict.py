1ba55f3c118ccc5d06a43b1c3110cbe114657b91
Fly me to the moon <git@bigly.dog>
2020-07-03 22:47:48 -0700

    parser

--- src/server/html_to_dict.py
+++ src/server/html_to_dict.py
@@ -1,8 +1,51 @@
+from html.parser import HTMLParser
 from typing import Any
 
 
+class Parser(HTMLParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(dir(self))
+
+    def handle_starttag(self, tag: str, attrs) -> None:
+        pass
+
+    def handle_endtag(self, tag: str) -> None:
+        pass
+
+    def handle_charref(self, name) -> None:
+        pass
+
+    def handle_entityref(self, name) -> None:
+        pass
+
+    def handle_data(self, data) -> None:
+        pass
+
+    def handle_comment(self, data) -> None:
+        pass
+
+    def handle_decl(self, decl) -> None:
+        pass
+
+    def handle_pi(self, data) -> None:
+        pass
+
+    def unknown_decl(self, data) -> None:
+        pass
+
+    def end(self) -> Any:
+        return ""
+        # return self.data
+
+
 def parse(html: str) -> Any:
-    return []
+    parser = Parser()
+    parser.feed(html)
+    data = parser.end()
+
+    print(data)
+    return data
 
 
 def unparse(data: Any) -> str:
