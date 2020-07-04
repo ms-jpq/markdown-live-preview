1ca74591186045a9b2899f20750c6d46f15fc864
Fly me to the moon <git@bigly.dog>
2020-07-04 00:34:59 -0700

    parse into node

--- src/server/html_to_dict.py
+++ src/server/html_to_dict.py
@@ -1,51 +1,64 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
 from html.parser import HTMLParser
-from typing import Any
+from typing import Any, List, Optional, Tuple, Union
+
+Attribute = Tuple[str, Optional[str]]
+
+
+class ParseError(Exception):
+    pass
+
+
+@dataclass
+class Node:
+    tag: str
+    attributes: List[Attribute] = field(default_factory=list)
+    children: List[Union[Node, str]] = field(default_factory=list)
 
 
 class Parser(HTMLParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        print(dir(self))
+        root = Node(tag="root")
+        self.__root = root
+        self.__stack: List[Node] = [root]
 
-    def handle_starttag(self, tag: str, attrs) -> None:
-        pass
+    def handle_starttag(self, tag: str, attrs: List[Attribute]) -> None:
+        node = Node(tag=tag, attributes=attrs)
+        if stack := self.__stack:
+            parent = stack[-1]
+            parent.children.append(node)
+            stack.append(node)
+        else:
+            raise ParseError()
 
     def handle_endtag(self, tag: str) -> None:
-        pass
+        if stack := self.__stack:
+            node = stack.pop()
+            if node.tag != tag:
+                raise ParseError()
+        else:
+            raise ParseError()
 
-    def handle_charref(self, name) -> None:
-        pass
+    def handle_data(self, data: str) -> None:
+        if stack := self.__stack:
+            parent = stack[-1]
+            parent.children.append(data)
+        else:
+            raise ParseError()
 
-    def handle_entityref(self, name) -> None:
-        pass
-
-    def handle_data(self, data) -> None:
-        pass
-
-    def handle_comment(self, data) -> None:
-        pass
-
-    def handle_decl(self, decl) -> None:
-        pass
-
-    def handle_pi(self, data) -> None:
-        pass
-
-    def unknown_decl(self, data) -> None:
-        pass
-
-    def end(self) -> Any:
-        return ""
-        # return self.data
+    def consume(self) -> Node:
+        return self.__root
 
 
-def parse(html: str) -> Any:
+def parse(html: str) -> Node:
     parser = Parser()
     parser.feed(html)
-    data = parser.end()
+    node = parser.consume()
 
-    print(data)
-    return data
+    return node
 
 
 def unparse(data: Any) -> str:
