36e0c0a0263ba9825483451f1a159b6122ca530b
Fly me to the moon <git@bigly.dog>
2020-07-04 21:16:49 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+from setuptools import setup
+
+
+def slurp(path: str) -> str:
+    with open(path) as fd:
+        return fd.read()
+
+
+setup(
+    name="markdown-live-preview",
+    version="1.0.0",
+    description="live web preview of markdown docs",
+    long_description=slurp("README.md"),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/markdown-live-preview",
+)
