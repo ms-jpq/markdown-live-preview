c260ffc285b693aed941e5ec054704cd9b32d7a4
Fly me to the moon <git@bigly.dog>
2020-07-05 02:06:12 -0700

    more efficient diff algo

--- markdown_live_preview/server/reconciliate.py
+++ markdown_live_preview/server/reconciliate.py
@@ -1,28 +1,48 @@
 from itertools import zip_longest
-from typing import Dict, Optional, Tuple, Union, cast
+from typing import Dict, List, Optional, Tuple, Union, cast
 
 from .html_to_dict import Node, parse, unparse
 
 
-def recon(prev: Union[Node, str, None], curr: Union[Node, str]) -> Union[Node, str]:
+def recon(
+    prev: Union[Node, str, None], curr: Union[Node, str]
+) -> Tuple[bool, Union[Node, str]]:
     if type(curr) == str:
-        return curr
+        return prev == curr, curr
     else:
+        p_node = type(prev) == Node
+
         curr = cast(Node, curr)
         depth = curr.depth
-        attrs: Dict[str, Optional[str]] = {**curr.attrs} if prev == curr else {
-            **curr.attrs,
-            "diff": "true",
-            "depth": str(depth),
-        }
-        p_children = cast(Node, prev).children if type(prev) == Node else []
-        children = [
+        p_children = cast(Node, prev).children if p_node else []
+        p_attrs = cast(Node, prev).attrs if p_node else []
+
+        diff = False
+
+        if len(p_children) != len(curr.children):
+            diff = True
+        elif p_attrs != curr.attrs:
+            diff = True
+
+        next_lv = (
             recon(p, c)
             for p, c in zip_longest(p_children, curr.children)
             if c is not None
-        ]
+        )
+
+        children: List[Union[Node, str]] = []
+        for d, c in next_lv:
+            children.append(c)
+            diff = diff or d
+
+        attrs: Dict[str, Optional[str]] = {
+            **curr.attrs,
+            "diff": "true",
+            "depth": str(depth),
+        } if diff else {**curr.attrs}
+
         node = Node(depth=depth, tag=curr.tag, attrs=attrs, children=children)
-        return node
+        return diff, node
 
 
 def reconciliate(prev: Optional[Node], curr: str) -> Tuple[Node, str]:
@@ -30,7 +50,7 @@ def reconciliate(prev: Optional[Node], curr: str) -> Tuple[Node, str]:
     if prev is None:
         return (nxt, curr)
     else:
-        marked = recon(prev, nxt)
+        _, marked = recon(prev, nxt)
         assert type(marked) == Node
         parsed = unparse(cast(Node, marked))
         return (nxt, parsed)
