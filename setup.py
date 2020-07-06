82d7844fa6500e47661d99f991d19d0bf7ccbe4e
Fly me to the moon <git@bigly.dog>
2020-07-06 16:59:28 -0700

    tag

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def slurp(path: str) -> str:
 
 setup(
     name="markdown-live-preview",
-    version="0.1.19",
+    version="0.1.20",
     description="live web preview of markdown docs",
     long_description=slurp("README.md"),
     long_description_content_type="text/markdown",
