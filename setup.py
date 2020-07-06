08390cf2ac720592302c498d855e2acdfec55b41
Fly me to the moon <git@bigly.dog>
2020-07-06 15:26:30 -0700

    man python has everything

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def slurp(path: str) -> str:
 
 setup(
     name="markdown-live-preview",
-    version="0.1.14",
+    version="0.1.15",
     description="live web preview of markdown docs",
     long_description=slurp("README.md"),
     long_description_content_type="text/markdown",
