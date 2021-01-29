1814628e1ddb14a1303105955991f629611d0a81
Fly me to the moon <git@bigly.dog>
2021-01-29 12:24:22 -0800

    build step

--- build.py
+++ build.py
@@ -3,18 +3,19 @@
 from locale import strxfrm
 from os import linesep
 from pathlib import Path
+from shutil import rmtree
 from subprocess import check_call
 
 from pygments.formatters.html import HtmlFormatter
 from pygments.styles import get_all_styles, get_style_by_name
 
 _TOP_LV = Path(__file__).resolve().parent
-_HL_CSS = _TOP_LV / ".cache" / "codehl.css"
 _CODEHL_CLASS = "codehilite"
 
 
 def main() -> None:
-    _HL_CSS.parent.mkdir(parents=True, exist_ok=True)
+    hl_css = _TOP_LV / ".cache" / "codehl.css"
+    hl_css.parent.mkdir(parents=True, exist_ok=True)
     lines = (
         f".{_CODEHL_CLASS}.{name} {line}"
         for name in get_all_styles()
@@ -23,11 +24,13 @@ def main() -> None:
         .splitlines()
     )
     css = linesep.join(sorted(lines, key=strxfrm))
-    _HL_CSS.write_text(css)
+    hl_css.write_text(css)
 
     node_bin = _TOP_LV / "node_modules" / ".bin"
     package = _TOP_LV / "markdown_live_preview"
+    js_dist = package / "js"
 
+    rmtree(js_dist)
     check_call((str(_TOP_LV / "lint.sh"),))
     check_call(
         (
@@ -35,7 +38,7 @@ def main() -> None:
             "build",
             "--no-source-maps",
             "--out-dir",
-            str(package / "js"),
+            str(js_dist),
             "--",
             str(package / "client" / "index.html"),
         )
--- markdown_live_preview/server/reconciliate.py
+++ markdown_live_preview/server/reconciliate.py
@@ -1,22 +1,20 @@
 from difflib import SequenceMatcher
-from typing import Callable, Sequence, Union
+from typing import Callable, Optional, Sequence, Union
 
 from .html_to_dict import Node, TextNode, parse, unparse
 
+_Nodes = Sequence[Union[Node, TextNode]]
+
 
 def reconciliate() -> Callable[[str], str]:
-    prev: Sequence[Union[Node, TextNode]] = ()
+    prev: Optional[Node] = None
 
-    def recon(xhtml: str) -> str:
-        nonlocal prev
-        root = parse(xhtml)
-        curr = tuple(root)
-
-        matcher = SequenceMatcher(isjunk=None, autojunk=False, a=curr, b=prev)
+    def diff_inplace(before: _Nodes, after: _Nodes) -> None:
+        matcher = SequenceMatcher(isjunk=None, autojunk=False, a=after, b=before)
         for group in matcher.get_grouped_opcodes():
             for op, i, j, _, _ in group:
                 if op == "replace":
-                    for node in curr[i:j]:
+                    for node in after[i:j]:
                         if isinstance(node, Node):
                             node.diff = True
                         else:
@@ -25,7 +23,7 @@ def reconciliate() -> Callable[[str], str]:
                                 parent.diff = True
 
                 elif op == "delete":
-                    for node in curr[i:j]:
+                    for node in after[i:j]:
                         if isinstance(node, Node):
                             node.diff = True
                         else:
@@ -34,7 +32,7 @@ def reconciliate() -> Callable[[str], str]:
                                 parent.diff = True
 
                 elif op == "insert":
-                    for node in curr[i:j]:
+                    for node in after[i:j]:
                         parent = node.parent and node.parent()
                         if parent:
                             parent.diff = True
@@ -45,7 +43,18 @@ def reconciliate() -> Callable[[str], str]:
                 else:
                     assert False
 
-        prev = curr
-        return unparse(root)
+    def recon(xhtml: str) -> str:
+        nonlocal prev
+        root = parse(xhtml)
+        if not prev:
+            return unparse(root)
+        else:
+            before, after = tuple(prev), tuple(root)
+            if before == after:
+                return unparse(prev)
+            else:
+                diff_inplace(before, after=after)
+                prev = root
+                return unparse(root)
 
     return recon
