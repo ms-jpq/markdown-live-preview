19c8f896ee12e7dd18b5610c82d054f07f0eda41
Fly me to the moon <git@bigly.dog>
2021-01-29 02:50:35 -0800

    html

--- build.py
+++ build.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from locale import strxfrm
+from os import linesep
 from pathlib import Path
 from subprocess import check_call
 
 from pygments.formatters.html import HtmlFormatter
-from pygments.styles import get_style_by_name
+from pygments.styles import get_all_styles, get_style_by_name
 
 _TOP_LV = Path(__file__).resolve().parent
 _HL_CSS = _TOP_LV / ".cache" / "codehl.css"
@@ -12,8 +14,14 @@ _HL_CSS = _TOP_LV / ".cache" / "codehl.css"
 
 def main() -> None:
     _HL_CSS.parent.mkdir(parents=True, exist_ok=True)
-    style = get_style_by_name("friendly")
-    css = HtmlFormatter(style=style).get_style_defs()
+    lines = (
+        f".{name} {line}"
+        for name in get_all_styles()
+        for line in HtmlFormatter(style=get_style_by_name(name))
+        .get_style_defs()
+        .splitlines()
+    )
+    css = linesep.join(sorted(lines, key=strxfrm))
     _HL_CSS.write_text(css)
 
     node_bin = _TOP_LV / "node_modules" / ".bin"
